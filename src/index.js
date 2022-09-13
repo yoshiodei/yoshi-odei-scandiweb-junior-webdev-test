8f6f29f814543a0e3f2fe176c2a396c998b0f156
yoshiodei <yoshiodei@yahoo.com>
2022-09-13 07:29:41 +0000

    add index files and styles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Routes from './Routes/Routes';
+import { ApolloProvider ,ApolloClient, InMemoryCache } from "@apollo/client";
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(), 
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Provider store={store}>  
+          <Routes />
+      </Provider>      
+    </ApolloProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
